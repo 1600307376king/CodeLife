1e7ab47b48c771ebf412efb09d3e368187126ee2
1600307376@qq.com <1600307376@qq.com>
2022-06-11 18:45:52 +0800

    update add note common config variable

---
+++ com_manager/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/11 17:04
+# @Author  : jjc
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ com_manager/note_dialog_common.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/11 17:05
+# @Author  : jjc
+# @File    : note_dialog_common.py
+# @Software: PyCharm
+
+ADD_NOTE_DIALOG_FONT = 18
+ADD_NOTE_DIALOG_WEIGHT = 75
+ADD_NOTE_DIALOG_FAMILY = "宋体"
+
+
+NOTE_DIALOG_SIZE = (1280, 960)
+
+TITLE_FRAME_MIN_SIZE = (1250, 50)
+TITLE_FRAME_MIN_HEIGHT = 50
+
+TITLE_NAME_LABEL_MAX_WIDTH = 100
+
+TITLE_EDIT_MIN_SIZE = (200, 30)
+TITLE_EDIT_MAX_SIZE = (200, 30)
+
+CONTENT_FRAME = (1250, 450)
+
+BUTTON_FRAME = (1250, 50)
+
+BTN_MAX_WIDTH = 100
+
+MAX_TITLE_INPUT_LEN = 20
+
